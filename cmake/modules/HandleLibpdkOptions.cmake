d99d610ddbaa2e39b834a8cf6ef100757df383e9
softboy <zzu_softboy@163.com>
2017-11-08 16:49:19 +0800

    add HandleLibpdkOptons cmake script

--- cmake/modules/HandleLibpdkOptions.cmake
+++ cmake/modules/HandleLibpdkOptions.cmake
@@ -150,7 +150,7 @@ endif()
 if(NOT WIN32 AND NOT CYGWIN)
     # MinGW warns if -fvisibility-inlines-hidden is used.
     check_cxx_compiler_flag("-fvisibility-inlines-hidden" PDK_TEMP_SUPPORTS_FVISIBILITY_INLINES_HIDDEN_FLAG)
-    adk_append_if(PDK_TEMP_SUPPORTS_FVISIBILITY_INLINES_HIDDEN_FLAG "-fvisibility-inlines-hidden" CMAKE_CXX_FLAGS)
+    pdk_append_if(PDK_TEMP_SUPPORTS_FVISIBILITY_INLINES_HIDDEN_FLAG "-fvisibility-inlines-hidden" CMAKE_CXX_FLAGS)
 endif()
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT WIN32)
@@ -242,8 +242,207 @@ if(MSVC)
     endif()
 endif()
 
-# choose CRT?
+if(PDK_COMPILER_IS_GCC_COMPATIBLE)
+    pdk_append_if(PDK_ENABLE_WERROR "-Werror" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+    pdk_append_if(LLVM_ENABLE_WERROR "-Wno-error" CMAKE_REQUIRED_FLAGS)
+    pdk_add_flag_if_supported("-Werror=date-time" WERROR_DATE_TIME)
+    pdk_add_flag_if_supported("-Werror=unguarded-availability-new" WERROR_UNGUARDED_AVAILABILITY_NEW)
+    check_cxx_compiler_flag("-std=c++1y" PDK_TEMP_CXX_SUPPORTS_CXX1Y)
+    pdk_append_if(PDK_TEMP_CXX_SUPPORTS_CXX1Y "-std=c++1y" CMAKE_CXX_FLAGS)
+    if(NOT PDK_TEMP_CXX_SUPPORTS_CXX1Y)
+        check_cxx_compiler_flag("-std=c++1z" PDK_TEMP_CXX_SUPPORTS_CXX1Z)
+        pdk_append_if(PDK_TEMP_CXX_SUPPORTS_CXX1Z "-std=c++1z" CMAKE_CXX_FLAGS)
+        if(NOT PDK_TEMP_CXX_SUPPORTS_CXX1Z)
+            check_cxx_compiler_flag("-std=c++11" PDK_TEMP_CXX_SUPPORTS_CXX11)
+            pdk_append_if(PDK_TEMP_CXX_SUPPORTS_CXX11 "-std=c++11" CMAKE_CXX_FLAGS)
+            if(PDK_TEMP_CXX_SUPPORTS_CXX11)
+                if (CYGWIN OR MINGW)
+                    # MinGW and Cygwin are a bit stricter and lack things like
+                    # 'strdup', 'stricmp', etc in c++11 mode.
+                    append("-std=gnu++11" CMAKE_CXX_FLAGS)
+                else()
+                    append("-std=c++11" CMAKE_CXX_FLAGS)
+                endif()
+            else()
+                message(FATAL_ERROR "libpdk requires C++11 support but the '-std=c++11' flag isn't supported.")
+            endif()
+        endif()
+    endif()
+    
+    if(PDK_ENABLE_MODULES)
+        set(PKD_TEMP_OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+        set(module_flags "-fmodules -fmodules-cache-path=${PROJECT_BINARY_DIR}/module.cache")
+        if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+            # On Darwin -fmodules does not imply -fcxx-modules.
+            set(module_flags "${module_flags} -fcxx-modules")
+        endif()
+        if(PDK_ENABLE_LOCAL_SUBMODULE_VISIBILITY)
+            set(module_flags "${module_flags} -Xclang -fmodules-local-submodule-visibility")
+        endif()
+        if (PDK_ENABLE_MODULE_DEBUGGING AND ((UPPERCASE_CMAKE_BUILD_TYPE STREQUAL "DEBUG") OR
+                (UPPERCASE_CMAKE_BUILD_TYPE STREQUAL "RELWITHDEBINFO")))
+            set(module_flags "${module_flags} -gmodules")
+        endif()
+        set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${module_flags}")
+        
+        # Check that we can build code with modules enabled, and that repeatedly
+        # including <cassert> still manages to respect NDEBUG properly.
+        check_cxx_source_compiles("#undef NDEBUG
+            #include <cassert>
+            #define NDEBUG
+            #include <cassert>
+            int main() { assert(this code is not compiled); }"
+            PDK_TEMP_CXX_SUPPORTS_MODULES)
+        set(CMAKE_REQUIRED_FLAGS ${PKD_TEMP_OLD_CMAKE_REQUIRED_FLAGS})
+        if(PDK_TEMP_CXX_SUPPORTS_MODULES)
+            pdk_append("${module_flags}" CMAKE_CXX_FLAGS)
+        else()
+            message(FATAL_ERROR "PDK_ENABLE_MODULES is not supported by this compiler")
+        endif()
+    endif(PDK_ENABLE_MODULES)
+endif(PDK_COMPILER_IS_GCC_COMPATIBLE)
 
+if(PDK_ENABLE_WARNINGS AND PDK_COMPILER_IS_GCC_COMPATIBLE)
+    pdk_append("-Wall -W -Wno-unused-parameter -Wwrite-strings" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+    pdk_append("-Wcast-qual" CMAKE_CXX_FLAGS)
+    # Turn off missing field initializer warnings for gcc to avoid noise from
+    # false positives with empty {}. Turn them on otherwise (they're off by
+    # default for clang).
+    check_cxx_compiler_flag("-Wmissing-field-initializers" PDK_TEMP_CXX_SUPPORTS_MISSING_FIELD_INITIALIZERS_FLAG)
+    if(PDK_TEMP_CXX_SUPPORTS_MISSING_FIELD_INITIALIZERS_FLAG)
+        if(CMAKE_COMPILER_IS_GNUCXX)
+            pdk_append("-Wno-missing-field-initializers" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+        else()
+            pdk_append("-Wmissing-field-initializers" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+        endif()
+    endif()
+    
+    if(PDK_ENABLE_PEDANTIC AND PDK_COMPILER_IS_GCC_COMPATIBLE)
+        pdk_append("-pedantic" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+        pdk_append("-Wno-long-long" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+    endif()
+    pdk_add_flag_if_supported("-Wcovered-switch-default" COVERED_SWITCH_DEFAULT_FLAG)
+    pdk_append_if(PDK_USE_NO_UNINITIALIZED "-Wno-uninitialized" CMAKE_CXX_FLAGS)
+    pdk_append_if(PDK_USE_NO_MAYBE_UNINITIALIZED "-Wno-maybe-uninitialized" CMAKE_CXX_FLAGS)
+    
+    # Check if -Wnon-virtual-dtor warns even though the class is marked final.
+    # If it does, don't add it. So it won't be added on clang 3.4 and older.
+    # This also catches cases when -Wnon-virtual-dtor isn't supported by
+    # the compiler at all.  This flag is not activated for gcc since it will
+    # incorrectly identify a protected non-virtual base when there is a friend
+    # declaration. Don't activate this in general on Windows as this warning has
+    # too many false positives on COM-style classes, which are destroyed with
+    # Release() (PR32286).
+    if(NOT CMAKE_COMPILER_IS_GNUCXX AND NOT WIN32)
+        set(PDK_TEMP_OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+        set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -std=c++11 -Werror=non-virtual-dtor")
+        check_cxx_source_compiles("class base {public: virtual void anchor();protected: ~base();};
+            class derived final : public base { public: ~derived();};
+            int main() { return 0; }"
+            PDK_TEMP_CXX_WONT_WARN_ON_FINAL_NONVIRTUALDTOR)
+        set(CMAKE_REQUIRED_FLAGS ${PDK_TEMP_OLD_CMAKE_REQUIRED_FLAGS})
+        pdk_append_if(PDK_TEMP_CXX_WONT_WARN_ON_FINAL_NONVIRTUALDTOR
+            "-Wnon-virtual-dtor" CMAKE_CXX_FLAGS)
+    endif()
+    # Enable -Wdelete-non-virtual-dtor if available.
+    pdk_add_flag_if_supported("-Wdelete-non-virtual-dtor" DELETE_NON_VIRTUAL_DTOR_FLAG)
+    # Check if -Wcomment is OK with an // comment ending with '\' if the next
+    # line is also a // comment.
+    set(PDK_TEMP_OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -Werror -Wcomment")
+    check_c_source_compiles("// \\\\\\n//\\nint main() {return 0;}"
+        PDK_TEMP_C_WCOMMENT_ALLOWS_LINE_WRAP)
+    set(CMAKE_REQUIRED_FLAGS ${PDK_TEMP_OLD_CMAKE_REQUIRED_FLAGS})
+    if (NOT PDK_TEMP_C_WCOMMENT_ALLOWS_LINE_WRAP)
+        pdk_append("-Wno-comment" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+    endif()
+    # Enable -Wstring-conversion to catch misuse of string literals.
+    pdk_add_flag_if_supported("-Wstring-conversion" STRING_CONVERSION_FLAG)
+    
+endif(PDK_ENABLE_WARNINGS AND PDK_COMPILER_IS_GCC_COMPATIBLE)
 
+if (PDK_COMPILER_IS_GCC_COMPATIBLE AND NOT PDK_ENABLE_WARNINGS)
+    pdk_append("-w" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+endif()
 
+add_definitions(-D__STDC_CONSTANT_MACROS)
+add_definitions(-D__STDC_FORMAT_MACROS)
+add_definitions(-D__STDC_LIMIT_MACROS)
+
+# clang doesn't print colored diagnostics when invoked from Ninja
+if (UNIX AND
+        CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND
+        CMAKE_GENERATOR STREQUAL "Ninja")
+    pdk_append("-fcolor-diagnostics" CMAKE_C_FLAGS CMAKE_CXX_FLAGS)
+endif()
+
+# lld doesn't print colored diagnostics when invoked from Ninja
+if(UNIX AND CMAKE_GENERATOR STREQUAL "Ninja")
+    pdk_check_linker_flag("-Wl,--color-diagnostics" PDK_TEMP_LINKER_SUPPORTS_COLOR_DIAGNOSTICS)
+    append_if(PDK_TEMP_LINKER_SUPPORTS_COLOR_DIAGNOSTICS "-Wl,--color-diagnostics"
+        CMAKE_EXE_LINKER_FLAGS CMAKE_MODULE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+endif()
+
+# Add flags for add_dead_strip().
+# FIXME: With MSVS, consider compiling with /Gy and linking with /OPT:REF?
+# But MinSizeRel seems to add that automatically, so maybe disable these
+# flags instead if LLVM_NO_DEAD_STRIP is set.
+if(NOT CYGWIN AND NOT WIN32)
+    if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Darwin" AND
+            NOT UPPERCASE_CMAKE_BUILD_TYPE STREQUAL "DEBUG")
+        check_c_compiler_flag("-Werror -fno-function-sections" PDK_TEMP_C_SUPPORTS_FNO_FUNCTION_SECTIONS)
+        if (PDK_TEMP_C_SUPPORTS_FNO_FUNCTION_SECTIONS)
+            # Don't add -ffunction-section if it can be disabled with -fno-function-sections.
+            # Doing so will break sanitizers.
+            pdk_add_flag_if_supported("-ffunction-sections" FFUNCTION_SECTIONS)
+        endif()
+        pdk_add_flag_if_supported("-fdata-sections" FDATA_SECTIONS)
+    endif()
+endif()
+
+if(MSVC)
+    # Remove flags here, for exceptions and RTTI.
+    # Each target property or source property should be responsible to control
+    # them.
+    # CL.EXE complains to override flags like "/GR /GR-".
+    string(REGEX REPLACE "(^| ) */EH[-cs]+ *( |$)" "\\1 \\2" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    string(REGEX REPLACE "(^| ) */GR-? *( |$)" "\\1 \\2" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+endif()
+
+if(PDK_ENABLE_LTO AND PDK_ON_WIN32 AND NOT PDK_LINKER_IS_LLD_LINK)
+    message(FATAL_ERROR "When compiling for Windows, PDK_ENABLE_LTO requires using lld as the linker (point CMAKE_LINKER at lld-link.exe)")
+endif()
+
+if(UPPERCASE_PDK_ENABLE_LTO STREQUAL "THIN")
+    pdk_append("-flto=thin" CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
+    if(NOT PDK_LINKER_IS_LLD_LINK)
+        pdk_append("-flto=thin" CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    endif()
+    # If the linker supports it, enable the lto cache. This improves initial build
+    # time a little since we re-link a lot of the same objects, and significantly
+    # improves incremental build time.
+    # FIXME: We should move all this logic into the clang driver.
+    if(APPLE)
+        pdk_append("-Wl,-cache_path_lto,${PROJECT_BINARY_DIR}/lto.cache"
+            CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    elseif(UNIX AND PDK_USE_LINKER STREQUAL "lld")
+        pdk_append("-Wl,--thinlto-cache-dir=${PROJECT_BINARY_DIR}/lto.cache"
+            CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    elseif(PDK_USE_LINKER STREQUAL "gold")
+        pdk_append("-Wl,--plugin-opt,cache-dir=${PROJECT_BINARY_DIR}/lto.cache"
+            CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    endif()
+elseif(UPPERCASE_LLVM_ENABLE_LTO STREQUAL "FULL")
+    pdk_append("-flto=full" CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
+    if(NOT PDK_LINKER_IS_LLD_LINK)
+        pdk_append("-flto=full" CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    endif()
+elseif(PDK_ENABLE_LTO)
+    pdk_append("-flto" CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
+    if(NOT PDK_LINKER_IS_LLD_LINK)
+        pdk_append("-flto" CMAKE_EXE_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS)
+    endif()
+endif()
+
+pdk_get_compile_definitions()
 
