f4522ba9066402e87d7d7f0815a8626b56d84606
softboy <zzu_softboy@163.com>
2017-11-29 12:04:11 +0800

    add pdk::os::thread::AtomicPointer

--- include/pdk/base/os/thread/Atomic.h
+++ include/pdk/base/os/thread/Atomic.h
@@ -16,4 +16,94 @@
 #ifndef PDK_M_BASE_OS_THREAD_ATOMIC_H
 #define PDK_M_BASE_OS_THREAD_ATOMIC_H
 
+#include "BasicAtomic.h"
+
+namespace pdk {
+namespace os {
+namespace thread {
+
+//PDK_WARNING_PUSH
+//PDK_WARNING_DISABLE_GCC("-Wextra")
+
+template <typename T>
+class AtomicInteger : public BasicAtomicInteger<T>
+{
+public:
+   constexpr AtomicInteger(T value = 0) noexcept
+      : BasicAtomicInteger<T>(value)
+   {}
+   
+   inline AtomicInteger(const AtomicInteger &other) noexcept
+      : BasicAtomicInteger<T>()
+   {
+      this->storeRelease(other.loadAcquire());
+   }
+   
+   AtomicInteger &operator=(const AtomicInteger &other) noexcept
+   {
+      this->storeRelease(other.loadAcquire());
+      return *this;
+   }
+};
+
+class AtomicInt : public AtomicInteger<int>
+{
+public:
+   AtomicInt(int value = 0) noexcept
+      : AtomicInteger<int>(value)
+   {}
+};
+
+template <typename T>
+class AtomicPointer : public BasicAtomicPointer<T>
+{
+public:
+   constexpr AtomicPointer(T *value = 0) noexcept
+      : BasicAtomicPointer<T>(value)
+   {}
+   AtomicPointer(const AtomicPointer &other) noexcept
+      : BasicAtomicPointer<T>()
+   {
+      this->storeRelease(other.loadAcquire());
+   }
+   
+   AtomicPointer &operator=(const AtomicPointer &other) noexcept
+   {
+      this->storeRelease(other.loadAcquire());
+      return *this;
+   }
+};
+
+//PDK_WARNING_POP
+
+template <typename T>
+inline void atomic_assign(T *&dest, T *source)
+{
+   if (dest == source) {
+      return;
+   }
+   source->ref.ref();
+   if (!dest->ref.deref()) {
+      delete dest;
+   }
+   dest = source;
+}
+
+template <typename T>
+inline void atomic_detach(T *&dest)
+{
+   if (dest->ref.load() == 1) {
+      return;
+   }
+   T *orig = dest;
+   dest = new T(*dest);
+   if (!orig->ref.deref()) {
+      delete orig;
+   }
+}
+
+} // thread
+} // os
+} // pdk
+
 #endif // PDK_M_BASE_OS_THREAD_ATOMIC_H
