271b34d572fc06d1312b48e2bea5af2521fc25fc
softboy <zzu_softboy@163.com>
2017-12-06 10:34:40 +0800

    add AlgorithmsTest::testPopulationCount testcase

--- unittests/kernel/AlgorithmsTest.cpp
+++ unittests/kernel/AlgorithmsTest.cpp
@@ -17,4 +17,88 @@
 #include "pdk/kernel/Algorithms.h"
 #include <list>
 #include <utility>
+#include <tuple>
+#include <cstdlib>
 
+namespace 
+{
+
+static constexpr const uint bitsSetInNibble[] = {
+   0, 1, 1, 2, 1, 2, 2, 3,
+   1, 2, 2, 3, 2, 3, 3, 4,
+};
+
+PDK_STATIC_ASSERT((sizeof bitsSetInNibble / sizeof *bitsSetInNibble) == 16);
+
+constexpr uint bits_set_in_byte(pdk::puint8 data)
+{
+   return bitsSetInNibble[data & 0xF] + bitsSetInNibble[data >> 4];
+}
+
+constexpr uint bits_set_in_short(pdk::puint16 data)
+{
+   return bits_set_in_byte(data & 0xFF) + bits_set_in_byte(data >> 8);
+}
+
+constexpr uint bits_set_in_int(pdk::puint32 data)
+{
+   return bits_set_in_short(data & 0xFFFF) + bits_set_in_short(data >> 16);
+}
+
+constexpr uint bits_set_in_int64(pdk::puint64 data)
+{
+   return bits_set_in_int(data & 0xFFFFFFFF) + bits_set_in_int(data >> 32);
+}
+
+}
+
+template <class T>
+class AlgorithmsTest : public testing::Test
+{};
+
+using TestTypes = testing::Types
+<pdk::puint8, pdk::puint16, pdk::puint32, pdk::puint64>;
+
+TYPED_TEST_CASE(AlgorithmsTest, TestTypes);
+
+TYPED_TEST(AlgorithmsTest, testPopulationCount)
+{
+   using DataType = std::list<std::tuple<pdk::puint64, uint>>;
+   size_t sizeTestType = sizeof(TypeParam);
+   DataType data;
+   for (uint i = 0; i < UCHAR_MAX; i++) {
+      const uchar byte = static_cast<uchar>(i);
+      const uint bits = bits_set_in_byte(byte);
+      const pdk::puint64 value = static_cast<pdk::puint64>(byte);
+      const pdk::puint64 input = value << ((i % sizeTestType) * 8U);
+      data.push_back(std::make_tuple(input, bits));
+   }
+   // @TODO not thread safe
+   std::srand(std::time(0));
+   // and some random ones:
+   if (sizeTestType >= 8) {
+      for (size_t i = 0; i < 1000; ++i) {
+         const pdk::puint64 input = static_cast<pdk::puint64>(std::rand()) << 32 | static_cast<pdk::puint32>(std::rand());
+         data.push_back(std::make_tuple(input, bits_set_in_int64(input)));
+      }
+   } else if (sizeTestType >= 2) {
+      for (size_t i = 0; i < 1000 ; ++i) {
+         const pdk::puint32 input = std::rand();
+         if (sizeTestType >= 4) {
+            data.push_back(std::make_tuple(static_cast<pdk::puint64>(input), bits_set_in_int(input)));
+         } else {
+            data.push_back(std::make_tuple(static_cast<pdk::puint64>(input & 0xFFFF), bits_set_in_short(input & 0xFFFF)));
+         }
+      }
+   }
+   DataType::iterator begin = data.begin();
+   DataType::iterator end = data.end();
+   while (begin != end) {
+      auto item = *begin;
+      pdk::puint64 input = std::get<0>(item);
+      uint expected = std::get<1>(item);
+      const TypeParam value = static_cast<TypeParam>(input);
+      ASSERT_EQ(pdk::kernel::population_count(value), expected);
+      ++begin;
+   }
+}
