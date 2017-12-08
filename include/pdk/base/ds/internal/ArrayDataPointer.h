16df2ca8acbc14eed12afadb114f95a161ec7277
softboy <zzu_softboy@163.com>
2017-12-08 23:07:31 +0800

    add pdk::ds::internal::ArrayDataPointer class

--- include/pdk/base/ds/internal/ArrayDataPointer.h
+++ include/pdk/base/ds/internal/ArrayDataPointer.h
@@ -16,6 +16,163 @@
 #ifndef PDK_M_BASE_DS_INTERNAL_ARRAYDATA_POINTER_H
 #define PDK_M_BASE_DS_INTERNAL_ARRAYDATA_POINTER_H
 
+#include "ArrayDataOperators.h"
 
+namespace pdk {
+namespace ds {
+namespace internal {
+
+template <typename T>
+class ArrayDataPointer
+{
+private:
+   using Data = TypedArrayData<T>;
+   using DataOperator = ArrayDataOperator<T>;
+   
+public:
+   ArrayDataPointer() noexcept
+      : m_data(Data::getSharedNull())
+   {}
+   
+   ArrayDataPointer(const ArrayDataPointer &other)
+      : m_data(other.m_data.m_ref.ref()
+               ? other.m_data
+               : other.clone(other.m_data.cloneFlags()))
+   {}
+   
+   explicit ArrayDataOperator(TypedArrayData<T> *ptr)
+      : m_data(ptr)
+   {
+      if (!ptr) {
+         throw new std::bad_alloc();
+      }
+   }
+   
+   ArrayDataPointer(ArrayDataPointerRef<T> ref)
+      : m_data(ref.m_ptr)
+   {}
+   
+   ArrayDataPointer &operator=(const ArrayDataPointer &other)
+   {
+      ArrayDataPointer temp(other);
+      this->swap(temp);
+      return *this;
+   }
+   
+   ArrayDataPointer(ArrayDataPointer &&other) noexcept
+      : m_data(other.m_data)
+   {
+      other.m_data = Data::getSharedNull();
+   }
+   
+   ArrayDataPointer &operator=(const ArrayDataPointer &&other) noexcept
+   {
+      ArrayDataPointer moved(std::move(other));
+      this->swap(moved);
+      return *this;
+   }
+   
+   DataOperator &operator*() const
+   {
+      PDK_ASSERT(m_data);
+      return *static_cast<DataOperator *>(m_data);
+   }
+   
+   DataOperator *operator->() const
+   {
+      PDK_ASSERT(m_data);
+      return static_cast<DataOperator *>(m_data);
+   }
+   
+   bool isNull() const
+   {
+      return m_data == Data::getSharedNull();
+   }
+   
+   Data *getData() const
+   {
+      return m_data;
+   }
+   
+   bool needsDetach() const
+   {
+      return (!m_data->isMutable() || m_data->m_ref.isShared());
+   }
+   
+#if !defined(PDK_NO_UNSHARABLE_CONTAINERS)
+   void setSharable(bool sharable)
+   {
+      if (needsDetach()) {
+         Data *detached = clone(sharable
+                                ? m_data->detachFlags() & ~ArrayData::Unsharable
+                                : m_data->detachFlags() | ArrayData::Unsharable);
+         ArrayDataPointer old(m_data);
+         m_data = detached;
+      } else {
+         m_data->m_ref.setSharable(sharable);
+      }
+   }
+#endif
+   
+   void swap(ArrayDataPointer &other) noexcept
+   {
+      std::swap(m_data, other.m_data);
+   }
+   
+   void clear()
+   {
+      ArrayDataPointer temp(m_data);
+      m_data = Data::getSharedNull();
+   }
+   
+   bool detach()
+   {
+      if (needsDetach()) {
+         Data *copy = clone(m_data->detachFlags());
+         ArrayDataPointer old(m_data);
+         m_data = copy;
+         return true;
+      }
+      return false;
+   }
+   
+   ~ArrayDataPointer()
+   {
+      if (m_data->m_ref.deref()) {
+         if (m_data->isMutable()) {
+            (*this)->destroyAll();
+         }
+         Data::deallocate(m_data);
+      }
+   }
+private:
+   Data *clone(ArrayData::AllocationOptions options) const PDK_REQUIRED_RESULT
+   {
+      Data *x = Data::allocate(m_data->detachCapacity(m_data->m_size), options);
+      if (!x) {
+         throw new std::bad_alloc();
+      }
+      ArrayDataPointer copy(x);
+      if (m_data->m_size) {
+         copy->copyAppend(m_data->begin(), m_data->end());
+      }
+      Data *result = copy.m_data;
+      copy.m_data = Data::getSharedNull();
+      return result;
+   }
+   
+private:
+   Data *m_data;
+};
+
+template <typename T>
+inline void swap(ArrayDataPointer<T> &left, ArrayDataPointer<T> &right)
+{
+   left.swap(right);
+}
+
+} // internal
+} // ds
+} // pdk
 
 #endif // PDK_M_BASE_DS_INTERNAL_ARRAYDATA_POINTER_H
