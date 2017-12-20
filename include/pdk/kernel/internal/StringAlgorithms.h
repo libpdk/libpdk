f51f1fba9713d34e13830aeb65ddf85bf413ba28
softboy <zzu_softboy@163.com>
2017-12-20 09:14:33 +0800

    add pdk::internal::StringAlgorithms class

--- include/pdk/kernel/internal/StringAlgorithms.h
+++ include/pdk/kernel/internal/StringAlgorithms.h
@@ -18,11 +18,110 @@
 #define PDK_KERNEL_STRING_ALGORITHMS_H
 
 #include "pdk/global/Global.h"
+#include "pdk/kernel/StringUtils.h" // for pdk::ascii_isspace
+#include "pdk/base/lang/Character.h"
+#include "pdk/global/EnumDefs.h"
 
 namespace pdk {
 namespace internal {
 
+using pdk::lang::Character;
 
+template <typename StringType>
+struct StringAlgorithms
+{
+   using CharType = typename StringType::value_type;
+   using SizeType = typename StringType::size_type;
+   using NakedStringType = typename std::remove_cv<StringType>::type;
+   
+   static const bool isConst = std::is_const<StringType>::value;
+   
+   static inline bool isSpace(char c)
+   {
+      return pdk::ascii_isspace(c);
+   }
+   
+   static inline bool isSpace(Character c)
+   {
+      return c.isSpace();
+   }
+   
+   static inline StringType trimmedInplace(NakedStringType &str, const CharType *begin, 
+                                           const CharType *end)
+   {
+      CharType *data = const_cast<CharType *>(str.cbegin());
+      if (begin != data) {
+         std::memmove(data, begin, (end - begin) * sizeof(CharType));
+      }
+      str.resize(end - begin);
+      return std::move(str);
+   }
+   
+   static inline void trimmedPositions(const CharType *&begin, const CharType *&end)
+   {
+      while (begin < end && isSpace(*begin)) {
+         ++begin;
+      }
+      if (begin < end) {
+         while (begin < end && isSpace(end[-1])) {
+            --end;
+         }
+      }
+   }
+   
+   static inline StringType trimmed(StringType &str)
+   {
+      const CharType *begin = str.cbegin();
+      const CharType *end = str.cend();
+      trimmedPositions(begin, end);
+      if (begin == str.cbegin() && end == str.cend()) {
+         return str;
+      }
+      if (!isConst && str.isDetached()) {
+         return trimmedInplace(str, begin, end);
+      }
+      return StringType(begin, end - begin);
+   }
+   
+   static inline StringType simplified(StringType &str)
+   {
+      if (str.isEmpty()) {
+         return str;
+      }
+      const CharType *src = str.cbegin();
+      const CharType *end = str.cend();
+      NakedStringType result = isConst || !str.isDetached() 
+            ? StringType(str.size(), pdk::Initialization::Uninitialized)
+            : std::move(str);
+      CharType *dest = const_cast<CharType *>(result.cbegin());
+      CharType *ptr = dest;
+      bool unmodified = true;
+      while (true) {
+         while (src != end && isSpace(*src)) {
+            ++src;
+         }
+         while (src != end && !isSpace(*src)) {
+            *ptr++ = *src++;
+         }
+         if (src == end) {
+            break;
+         }
+         if (*src != Character::Space) {
+            unmodified = false;
+         }
+         *ptr++ = Character::Space;
+      }
+      if (ptr != dest && ptr[-1] == Character::Space) {
+         --ptr;
+      }
+      int newLength = ptr - dest;
+      if (isConst && newLength == str.size() && unmodified) {
+         return str;
+      }
+      result.resize(newLength);
+      return result;
+   }
+};
 
 } // internal 
 } // pdk
