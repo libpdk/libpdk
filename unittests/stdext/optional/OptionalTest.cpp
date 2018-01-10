41c4620affe6caa4ceda1a141c551e984fac2b71
softboy <zzu_softboy@163.com>
2018-01-10 18:10:45 +0800

    add OptionalTest::testWithClassTypes testcase

--- unittests/stdext/optional/OptionalTest.cpp
+++ unittests/stdext/optional/OptionalTest.cpp
@@ -349,7 +349,7 @@ void test_relops( T const* )
 template<class T>
 void test_none( T const* )
 {
-   TRACE( std::endl << BOOST_CURRENT_FUNCTION   );
+   TRACE( std::endl << BOOST_CURRENT_FUNCTION  );
    
    using pdk::stdext::none;
    
@@ -373,25 +373,219 @@ void test_none( T const* )
 
 struct VBase : virtual X
 {
-    VBase(int v) : X(v) {}
-    // MSVC 8.0 doesn't generate this correctly...
-    VBase(const VBase& other) : X(static_cast<const X&>(other)) {}
+   VBase(int v) : X(v) {}
+   // MSVC 8.0 doesn't generate this correctly...
+   VBase(const VBase& other) : X(static_cast<const X&>(other)) {}
 };
 
-//TEST(OptionalTest, testWithBuiltinTypes)
-//{
-//   test_basics(ARG(double));
-//   test_conditional_ctor_and_get_valur_or(ARG(double));
-//   test_uninitialized_access(ARG(double));
-//   test_no_throwing_swap(ARG(double));
-//   test_relops(ARG(double));
-//   test_none(ARG(double));
-//}
+template<class T>
+void test_direct_value_manip( T const* )
+{
+   T x(3);
+   
+   Optional<T> const c_opt0(x);
+   Optional<T> opt0(x);
+   
+   ASSERT_TRUE(c_opt0.get().V() == x.V());
+   ASSERT_TRUE(opt0.get().V() == x.V());
+   
+   ASSERT_TRUE(c_opt0->V() == x.V());
+   ASSERT_TRUE(opt0->V() == x.V());
+   
+   ASSERT_TRUE((*c_opt0).V() == x.V());
+   ASSERT_TRUE((*  opt0).V() == x.V());
+   
+   T y(4);
+   opt0 = y ;
+   ASSERT_TRUE(opt0.get().V() == y.V());
+}
+
+template<class T>
+void test_throwing_direct_init( T const* )
+{
+   
+   T a(6);
+   int count = get_instance_count( ARG(T));
+   set_throw_on_copy( ARG(T));
+   bool passed = false ;
+   try
+   {
+      // This should:
+      //   Attempt to copy construct 'a' and throw.
+      // 'opt' won't be constructed.
+      set_pending_copy(ARG(T)) ;
+      Optional<T> opt(a) ;
+      passed = true ;
+   }
+   catch(... ){}
+   
+   ASSERT_TRUE(!passed);
+   check_is_not_pending_copy(ARG(T));
+   check_instance_count(count, ARG(T));
+   reset_throw_on_copy(ARG(T));
+}
+
+template<class T>
+void test_throwing_copy_initialization( T const* )
+{
+   T z(0);
+   T a(10);
+   Optional<T> opt (a);
+   int count = get_instance_count(ARG(T));
+   set_throw_on_copy(ARG(T));
+   bool passed = false ;
+   try
+   {
+      // This should:
+      //   Attempt to copy construct 'opt' and throw.
+      //   opt1 won't be constructed.
+      set_pending_copy(ARG(T));
+      Optional<T> opt1 = opt ;
+      passed = true ;
+   }
+   catch(... ) {}
+   ASSERT_TRUE(!passed);
+   check_is_not_pending_copy(ARG(T));
+   check_instance_count(count, ARG(T));
+   // Nothing should have happened to the source optional.
+   check_initialized(opt);
+   check_value(opt, a, z);
+   reset_throw_on_copy(ARG(T));
+}
+
+template<class T>
+void test_throwing_assign_to_uninitialized( T const* )
+{
+   T z(0);
+   T a(11);
+   Optional<T> opt0 ;
+   Optional<T> opt1(a) ;
+   int count = get_instance_count(ARG(T));
+   set_throw_on_copy(ARG(T));
+   bool passed = false ;
+   try
+   {
+      // This should:
+      //   Attempt to copy construct 'opt1.value()' into opt0 and throw.
+      //   opt0 should be left uninitialized.
+      set_pending_copy(ARG(T));
+      opt0 = opt1 ;
+      passed = true ;
+   }
+   catch(... ) {}
+   
+   ASSERT_TRUE(!passed);
+   check_is_not_pending_copy(ARG(T));
+   check_instance_count(count, ARG(T));
+   check_uninitialized(opt0);
+   reset_throw_on_copy(ARG(T));
+}
+
+template<class T>
+void test_throwing_assign_to_initialized( T const* )
+{
+   T z(0);
+   T a(12);
+   T b(13);
+   T x(-1);
+   Optional<T> opt0(a);
+   Optional<T> opt1(b);
+   int count = get_instance_count(ARG(T));
+   set_throw_on_assign( ARG(T));
+   bool passed = false;
+   try
+   {
+      // This should:
+      //   Attempt to copy construct 'opt1.value()' into opt0 and throw.
+      //   opt0 is kept initialized but its value not neccesarily fully assigned
+      //   (in this test, incompletely assigned is flaged with the value -1 being set)
+      set_pending_assign(ARG(T));
+      opt0 = opt1;
+      passed = true;
+   }
+   catch(... ) {}
+   
+   ASSERT_TRUE(!passed);
+   // opt0 was left uninitialized
+   check_is_not_pending_assign(ARG(T));
+   check_instance_count(count, ARG(T));
+   check_initialized(opt0);
+   check_value(opt0, x, z);
+   reset_throw_on_assign( ARG(T));
+}
+
+template<class T>
+void test_throwing_swap( T const* )
+{
+   T a(16);
+   T b(17);
+   T x(-1);
+   
+   Optional<T> opt0(a) ;
+   Optional<T> opt1(b) ;
+   
+   set_throw_on_assign(ARG(T));
+   
+   //
+   // Case 1: Both Initialized.
+   //
+   bool passed = false;
+   try
+   {
+      // This should attempt to swap optionals and fail at swap(X&,X&).
+      swap(opt0, opt1);
+      passed = true ;
+   }
+   catch(... ) {}
+   
+   ASSERT_TRUE(!passed);
+   
+   // optional's swap doesn't affect the initialized states of the arguments. Therefore,
+   // the following must hold:
+   check_initialized(opt0);
+   check_initialized(opt1);
+   check_value(opt0, x, a);
+   check_value(opt1, b, x);
+   //
+   // Case 2: Only one Initialized.
+   //
+   reset_throw_on_assign(ARG(T));
+   
+   set_throw_on_copy(ARG(T));
+   
+   passed = false;
+   opt0.~Optional();
+   try
+   {
+      // This should attempt to swap optionals and fail at opt0.reset(*opt1)
+      // Both opt0 and op1 are left unchanged (unswaped)
+      swap(opt0, opt1);
+      passed = true ;
+   }
+   catch(... ) {}
+   ASSERT_TRUE(!passed);
+   check_uninitialized(opt0);
+   check_initialized(opt1);
+   check_value(opt1, b, x);
+   reset_throw_on_copy(ARG(T));
+}
+
 
 TEST(OptionalTest, testWithClassTypes)
 {
-//   Optional<X> a(1);
-//   Optional<X> b(3);
+   //   Optional<X> a(1);
+   //   Optional<X> b(3);
    test_basics(ARG(X));
    test_basics(ARG(VBase));
+   test_conditional_ctor_and_get_valur_or(ARG(X));
+   test_direct_value_manip(ARG(X));
+   test_uninitialized_access(ARG(X));
+   test_throwing_direct_init(ARG(X));
+   test_throwing_copy_initialization(ARG(X));
+   test_throwing_assign_to_uninitialized(ARG(X));
+   test_throwing_assign_to_initialized(ARG(X));
+   test_throwing_swap(ARG(X));
+   test_relops(ARG(X));
+   test_none(ARG(X));
+   ASSERT_TRUE(X::m_count == 0);
 }
