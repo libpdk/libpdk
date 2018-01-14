3a8d900b1787c71ebd2274e372a32abbc705a5d3
softboy <zzu_softboy@163.com>
2018-01-14 17:17:51 +0800

    add include/pdk/stdext/preprocessor/config/Limits.h

---
+++ include/pdk/stdext/preprocessor/config/Limits.h
@@ -0,0 +1,43 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/14.
+
+// ****************************************************************************
+// *                                                                          *
+// *     (C) Copyright Paul Mensonides 2002-2011.                             *
+// *     (C) Copyright Edward Diener 2011.                                    *
+// *     Distributed under the Boost Software License, Version 1.0. (See      *
+// *     accompanying file LICENSE_1_0.txt or copy at                         *
+// *     http://www.boost.org/LICENSE_1_0.txt)                                *
+// *                                                                          *
+// ****************************************************************************
+// 
+// See http://www.boost.org for most recent version.
+
+#ifndef PDK_STDEXT_PREPROCESSOR_CONFIG_LIMITS_H
+#define PDK_STDEXT_PREPROCESSOR_CONFIG_LIMITS_H
+
+# define PDK_PP_LIMIT_MAG 256
+# define PDK_PP_LIMIT_TUPLE 64
+# define PDK_PP_LIMIT_DIM 3
+# define PDK_PP_LIMIT_REPEAT 256
+# define PDK_PP_LIMIT_WHILE 256
+# define PDK_PP_LIMIT_FOR 256
+# define PDK_PP_LIMIT_ITERATION 256
+# define PDK_PP_LIMIT_ITERATION_DIM 3
+# define PDK_PP_LIMIT_SEQ 256
+# define PDK_PP_LIMIT_SLOT_SIG 10
+# define PDK_PP_LIMIT_SLOT_COUNT 5
+
+#endif // PDK_STDEXT_PREPROCESSOR_CONFIG_LIMITS_H
