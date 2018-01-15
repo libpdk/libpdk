508bb17e777a540a7285d63f00ce2614673f9a24
softboy <zzu_softboy@163.com>
2018-01-15 14:05:37 +0800

    add PDK_PP_VARIADIC_TO_TUPLE macro

---
+++ include/pdk/stdext/preprocessor/variadic/ToTuple.h
@@ -0,0 +1,39 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/15.
+
+// ****************************************************************************
+// *                                                                          *
+// *     (C) Copyright Paul Mensonides 2002-2011.                             *
+// *     (C) Copyright Edward Diener 2011.                                    *
+// *     Distributed under the Boost Software License, Version 1.0. (See      *
+// *     accompanying file LICENSE_1_0.txt or copy at                         *
+// *     http://www.boost.org/LICENSE_1_0.txt)                                *
+// *                                                                          *
+// ****************************************************************************
+// 
+// See http://www.boost.org for most recent version.
+
+#ifndef PDK_STDEXT_PREPROCESSOR_VARIADIC_TO_TUPLE_H
+#define PDK_STDEXT_PREPROCESSOR_VARIADIC_TO_TUPLE_H
+
+#include "pdk/stdext/preprocessor/config/Config.h"
+
+// PDK_PP_VARIADIC_TO_TUPLE
+
+#if PDK_PP_VARIADICS
+#    define PDK_PP_VARIADIC_TO_TUPLE(...) (__VA_ARGS__)
+# endif
+
+#endif // PDK_STDEXT_PREPROCESSOR_VARIADIC_TO_TUPLE_H
