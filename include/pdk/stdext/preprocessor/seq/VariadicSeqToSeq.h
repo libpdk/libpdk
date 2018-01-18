f80857e33452cbd7bc279a55068881c86f1108bb
softboy <zzu_softboy@163.com>
2018-01-18 11:46:05 +0800

    add PDK_PP_VARIADIC_SEQ_TO_SEQ macro

---
+++ include/pdk/stdext/preprocessor/seq/VariadicSeqToSeq.h
@@ -0,0 +1,43 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/18.
+
+// ****************************************************************************
+// *                                                                          *
+// *     (C) Copyright Paul Mensonides 2002-2011.                             *
+// *     (C) Copyright Edward Diener 2011.                                    *
+// *     Distributed under the Boost Software License, Version 1.0. (See      *
+// *     accompanying file LICENSE_1_0.txt or copy at                         *
+// *     http://www.boost.org/LICENSE_1_0.txt)                                *
+// *                                                                          *
+// ****************************************************************************
+// 
+// See http://www.boost.org for most recent version.
+
+#ifndef PDK_STDEXT_PREPROCESSOR_SEQ_VARIADIC_SEQ_TO_SEQ_H
+#define PDK_STDEXT_PREPROCESSOR_SEQ_VARIADIC_SEQ_TO_SEQ_H
+
+#include "pdk/stdext/preprocessor/Cat.h"
+#include "pdk/stdext/preprocessor/config/Config.h"
+
+// PDK_PP_VARIADIC_SEQ_TO_SEQ
+# if PDK_PP_VARIADICS
+#    define PDK_PP_VARIADIC_SEQ_TO_SEQ(vseq) PDK_PP_CAT(PDK_PP_VARIADIC_SEQ_TO_SEQ_A vseq, 0)
+#    define PDK_PP_VARIADIC_SEQ_TO_SEQ_A(...) ((__VA_ARGS__)) PDK_PP_VARIADIC_SEQ_TO_SEQ_B
+#    define PDK_PP_VARIADIC_SEQ_TO_SEQ_B(...) ((__VA_ARGS__)) PDK_PP_VARIADIC_SEQ_TO_SEQ_A
+#    define PDK_PP_VARIADIC_SEQ_TO_SEQ_A0
+#    define PDK_PP_VARIADIC_SEQ_TO_SEQ_B0
+# endif
+
+#endif // PDK_STDEXT_PREPROCESSOR_SEQ_VARIADIC_SEQ_TO_SEQ_H
