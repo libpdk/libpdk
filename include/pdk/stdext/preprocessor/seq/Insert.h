a72d0b70f52570d62e689021379759f8a3c7b4fd
softboy <zzu_softboy@163.com>
2018-01-18 10:37:44 +0800

    add PDK_PP_SEQ_INSERT macro

---
+++ include/pdk/stdext/preprocessor/seq/Insert.h
@@ -0,0 +1,43 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/18.
+
+// ****************************************************************************
+// *                                                                          *
+// *     (C) Copyright Paul Mensonides 2002-2011.                             *
+// *     (C) Copyright Edward Diener 2011.                                    *
+// *     Distributed under the Boost Software License, Version 1.0. (See      *
+// *     accompanying file LICENSE_1_0.txt or copy at                         *
+// *     http://www.boost.org/LICENSE_1_0.txt)                                *
+// *                                                                          *
+// ****************************************************************************
+// 
+// See http://www.boost.org for most recent version.
+
+#ifndef PDK_STDEXT_PREPROCESSOR_SEQ_INSERT_H
+#define PDK_STDEXT_PREPROCESSOR_SEQ_INSERT_H
+
+#include "pdk/stdext/preprocessor/config/Config.h"
+#include "pdk/stdext/preprocessor/seq/FirstN.h"
+#include "pdk/stdext/preprocessor/seq/RestN.h"
+
+// PDK_PP_SEQ_INSERT
+# if ~PDK_PP_CONFIG_FLAGS() & PDK_PP_CONFIG_EDG()
+#    define PDK_PP_SEQ_INSERT(seq, i, elem) PDK_PP_SEQ_FIRST_N(i, seq) (elem) PDK_PP_SEQ_REST_N(i, seq)
+# else
+#    define PDK_PP_SEQ_INSERT(seq, i, elem) PDK_PP_SEQ_INSERT_I(seq, i, elem)
+#    define PDK_PP_SEQ_INSERT_I(seq, i, elem) PDK_PP_SEQ_FIRST_N(i, seq) (elem) PDK_PP_SEQ_REST_N(i, seq)
+# endif
+
+#endif // PDK_STDEXT_PREPROCESSOR_SEQ_INSERT_H
