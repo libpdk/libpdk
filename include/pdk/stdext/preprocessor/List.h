8a055d7cbff1d0774898743957a1d40fd62a9a61
softboy <zzu_softboy@163.com>
2018-01-18 14:59:14 +0800

    add PDK_STDEXT_PREPROCESSOR_LOGICAL_H macro

---
+++ include/pdk/stdext/preprocessor/List.h
@@ -0,0 +1,51 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/15.
+
+// ****************************************************************************
+// *                                                                          *
+// *     (C) Copyright Paul Mensonides 2002-2011.                             *
+// *     (C) Copyright Edward Diener 2011.                                    *
+// *     Distributed under the Boost Software License, Version 1.0. (See      *
+// *     accompanying file LICENSE_1_0.txt or copy at                         *
+// *     http://www.boost.org/LICENSE_1_0.txt)                                *
+// *                                                                          *
+// ****************************************************************************
+// 
+// See http://www.boost.org for most recent version.
+
+#ifndef PDK_STDEXT_PREPROCESSOR_LIST_H
+#define PDK_STDEXT_PREPROCESSOR_LIST_H
+
+#include "pdk/stdext/preprocessor/list/Adt.h"
+#include "pdk/stdext/preprocessor/list/Append.h"
+#include "pdk/stdext/preprocessor/list/At.h"
+#include "pdk/stdext/preprocessor/list/Cat.h"
+#include "pdk/stdext/preprocessor/list/Enum.h"
+#include "pdk/stdext/preprocessor/list/Filter.h"
+#include "pdk/stdext/preprocessor/list/FirstN.h"
+#include "pdk/stdext/preprocessor/list/FoldLeft.h"
+#include "pdk/stdext/preprocessor/list/FoldRight.h"
+#include "pdk/stdext/preprocessor/list/ForEach.h"
+#include "pdk/stdext/preprocessor/list/ForEachI.h"
+#include "pdk/stdext/preprocessor/list/ForEachProduct.h"
+#include "pdk/stdext/preprocessor/list/RestN.h"
+#include "pdk/stdext/preprocessor/list/Reverse.h"
+#include "pdk/stdext/preprocessor/list/Size.h"
+#include "pdk/stdext/preprocessor/list/ToArray.h"
+#include "pdk/stdext/preprocessor/list/ToSeq.h"
+#include "pdk/stdext/preprocessor/list/ToTuple.h"
+#include "pdk/stdext/preprocessor/list/Transform.h"
+
+#endif // PDK_STDEXT_PREPROCESSOR_LIST_H
