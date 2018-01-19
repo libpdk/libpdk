04a71aab61e07f3f07c9b667da96ae0a54170168
softboy <zzu_softboy@163.com>
2018-01-19 10:12:34 +0800

    add pdk::kernel::signal class

---
+++ include/pdk/kernel/signal/SignalBase.h
@@ -0,0 +1,53 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/19.
+
+// Boost.Signals2 library
+
+// Copyright Frank Mori Hess 2007-2008.
+// Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org
+
+#ifndef PDK_KERNEL_SIGNAL_SIGNAL_BASE_H
+#define PDK_KERNEL_SIGNAL_SIGNAL_BASE_H
+
+#include "pdk/global/Global.h"
+#include <memory>
+
+namespace pdk {
+namespace kernel {
+namespace signal {
+
+class SlotBase;
+
+class SignalBase
+{
+public:
+   friend class SlotBase;
+   virtual ~SignalBase() = default;
+protected:
+   virtual std::shared_ptr<void> getLockPimpl() const = 0;
+private:
+   PDK_DISABLE_COPY(SignalBase);
+};
+
+} // signal
+} // kernel
+} // pdk
+
+#endif // PDK_KERNEL_SIGNAL_SIGNAL_BASE_H
