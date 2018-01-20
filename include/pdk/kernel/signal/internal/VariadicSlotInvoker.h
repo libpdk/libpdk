2bd3a68307e364881e01616a13986970e158f25f
softboy <zzu_softboy@163.com>
2018-01-20 16:12:37 +0800

    add UnsignedMetaArray class

--- include/pdk/kernel/signal/internal/VariadicSlotInvoker.h
+++ include/pdk/kernel/signal/internal/VariadicSlotInvoker.h
@@ -21,3 +21,122 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 // For more information, see http://www.boost.org
+
+#ifndef PDK_KERNEL_SIGNAL_INTERNAL_VARIADIC_SLOT_INVOKER_H
+#define PDK_KERNEL_SIGNAL_INTERNAL_VARIADIC_SLOT_INVOKER_H
+
+#include "pdk/global/Global.h"
+#include "pdk/kernel/signal/internal/VariadicArgType.h"
+#include <tuple>
+
+#ifdef PDK_CC_MSVC
+#pragma warning(push)
+#if  PDK_CC_MSVC >= 1800
+#pragma warning(disable:4100)
+#endif
+#endif
+
+template<unsigned ... values> 
+class UnsignedMetaArray
+{};
+
+template<typename UnsignedMetaArray, unsigned n>
+class UnsignedMetaArrayAppender;
+
+template<unsigned n, unsigned ... Args>
+class UnsignedMetaArrayAppender<UnsignedMetaArray<Args...>, n>
+{
+public:
+   using type = UnsignedMetaArray<Args..., n>;
+};
+
+template<unsigned n> 
+class MakeUnsignedMetaArray;
+
+template<> 
+class MakeUnsignedMetaArray<0>
+{
+public:
+   using type = UnsignedMetaArray<>;
+};
+
+template<> 
+class MakeUnsignedMetaArray<1>
+{
+public:
+   using type = UnsignedMetaArray<0>;
+};
+
+template<unsigned n>
+class MakeUnsignedMetaArray
+{
+public:
+   using type = typename UnsignedMetaArrayAppender<typename MakeUnsignedMetaArray<n - 1>::type, n - 1>::type;
+};
+
+template<typename R>
+class CallWithTupleArgs
+{
+public:
+   typedef R result_type;
+   
+   template<typename Func, typename ... Args, std::size_t N>
+   R operator()(Func &func, const std::tuple<Args...> & args, std::integral_constant<std::size_t, N>) const
+   {
+      typedef typename MakeUnsignedMetaArray<N>::type indices_type;
+      return m_invoke<Func>(func, indices_type(), args);
+   }
+private:
+   template<typename Func, unsigned ... indices, typename ... Args>
+   R m_invoke(Func &func, UnsignedMetaArray<indices...>, const std::tuple<Args...> &args,
+              typename boost::disable_if<boost::is_void<typename Func::result_type> >::type * = 0
+         ) const
+   {
+      return func(BOOST_SIGNALS2_GET<indices>(args)...);
+   }
+   template<typename Func, unsigned ... indices, typename ... Args>
+   R m_invoke(Func &func, UnsignedMetaArray<indices...>, const std::tuple<Args...> &args,
+              typename boost::enable_if<boost::is_void<typename Func::result_type> >::type * = 0
+         ) const
+   {
+      func(std::get<indices>(args)...);
+      return R();
+   }
+   // This overload is redundant, as it is the same as the previous variadic method when
+   // it has zero "indices" or "Args" variadic template parameters.  This overload
+   // only exists to quiet some unused parameter warnings
+   // on certain compilers (some versions of gcc and msvc)
+   template<typename Func>
+   R m_invoke(Func &func, unsigned_meta_array<>, const std::tuple<> &, 
+              typename boost::enable_if<boost::is_void<typename Func::result_type> >::type * = 0
+         ) const
+   {
+      func();
+      return R();
+   }
+};
+
+template<typename R, typename ... Args>
+class VariadicSlotInvoker
+{
+public:
+   typedef R result_type;
+   
+   VariadicSlotInvoker(Args & ... args): _args(args...)
+   {}
+   
+   template<typename ConnectionBodyType>
+   result_type operator ()(const ConnectionBodyType &connectionBody) const
+   {
+      return CallWithTupleArgs<result_type>()(connectionBody->slot().slot_function(), 
+                                              _args, mpl::size_t<sizeof...(Args)>());
+   }
+private:
+   std::tuple<Args& ...> _args;
+};
+
+#ifdef PDK_CC_MSVC
+#pragma warning(pop)
+#endif
+
+#endif // PDK_KERNEL_SIGNAL_INTERNAL_VARIADIC_SLOT_INVOKER_H
