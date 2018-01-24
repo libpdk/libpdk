1f76d2ea16bf7ce51fa81d88c4c46a6a6cb32e82
softboy <zzu_softboy@163.com>
2018-01-24 19:08:22 +0800

    add pdk::kernel::AbstractEventDispatcher class apis

--- include/pdk/kernel/AbstractEventDispatcher.h
+++ include/pdk/kernel/AbstractEventDispatcher.h
@@ -18,11 +18,80 @@
 
 #include "pdk/kernel/Object.h"
 #include "pdk/kernel/EventLoop.h"
+#include <list>
 
 namespace pdk {
+
+// forward declare class with namespace
+namespace ds {
+class ByteArray;
+} // ds
+
 namespace kernel {
 
+// forward declare class with namespace
+namespace internal {
+class AbstractEventDispatcherPrivate;
+} // internal
 
+class AbstractNativeEventFilter;
+class SocketNotifier;
+using pdk::ds::ByteArray;
+using internal::AbstractEventDispatcherPrivate;
+
+class PDK_CORE_EXPORT AbstractEventDispatcher : public Object
+{
+   PDK_DECLARE_PRIVATE(AbstractEventDispatcher);
+public:
+   struct TimerInfo
+   {
+      inline TimerInfo(int id, int i, pdk::TimerType t)
+         : m_timerId(id),
+           m_interval(i),
+           m_timerType(t)
+      {}
+      int m_timerId;
+      int m_interval;
+      pdk::TimerType m_timerType;
+   };
+   
+   explicit AbstractEventDispatcher(Object *parent = nullptr);
+   ~AbstractEventDispatcher();
+   
+   static AbstractEventDispatcher *instance(Thread *thread = nullptr);
+   virtual bool processEvents(EventLoop::ProcessEventsFlags flags) = 0;
+   virtual void registerSocketNotifier(SocketNotifier *notifier) = 0;
+   virtual void unregisterSocketNotifier(SocketNotifier *notifier) = 0;
+   int registerTimer(int interval, pdk::TimerType timerType, Object *object);
+   virtual void registerTimer(int timerId, int interval, pdk::TimerType timerType, Object *object) = 0;
+   virtual bool unregisterTimer(int timerId) = 0;
+   virtual bool unregisterTimers(Object *object) = 0;
+   virtual std::list<TimerInfo> registeredTimers(Object *object) const = 0;
+   virtual int remainingTime(int timerId) = 0;
+   
+#ifdef PDK_OS_WIN
+   virtual bool registerEventNotifier(WinEventNotifier *notifier) = 0;
+   virtual void unregisterEventNotifier(WinEventNotifier *notifier) = 0;
+#endif
+   
+   virtual void wakeUp() = 0;
+   virtual void interrupt() = 0;
+   virtual void flush() = 0;
+   virtual void startingUp();
+   virtual void closingDown();
+   
+   void installNativeEventFilter(AbstractNativeEventFilter *filterObj);
+   void removeNativeEventFilter(AbstractNativeEventFilter *filterObj);
+   bool filterNativeEvent(const ByteArray &eventType, void *message, long *result);
+   
+   // signal
+   // void aboutToBlock();
+   // void awake();
+protected:
+   virtual bool hasPendingEvents() = 0;
+   AbstractEventDispatcher(AbstractEventDispatcherPrivate &,
+                           Object *parent);
+};
 
 } // kernel
 } // pdk
