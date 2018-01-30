e3801447b7c20b1a1373d16e65d06cb4bf2aa4d7
softboy <zzu_softboy@163.com>
2018-01-30 22:48:45 +0800

    add pdk::utils::SharedData class

--- include/pdk/base/time/TimeZone.h
+++ include/pdk/base/time/TimeZone.h
@@ -18,12 +18,141 @@
 
 #include "pdk/utils/Locale.h"
 #include "pdk/base/time/DateTime.h"
+#include "pdk/utils/SharedData.h"
+
+#include <vector>
+
+PDK_REQUIRE_CONFIG(TIME_ZONE);
 
 namespace pdk {
+
+// forward declare class with namespace
+namespace ds {
+class ByteArray;
+} // ds
+
 namespace time {
 
-using pdk::utils::Locale;
+// forward declare class with namespace
+namespace internal {
+class TimeZonePrivate;
+class DateTimePrivate;
+} // internal
 
+using pdk::utils::Locale;
+using internal::TimeZonePrivate;
+using internal::DateTimePrivate;
+using pdk::lang::String;
+using pdk::ds::ByteArray;
+
+class PDK_CORE_EXPORT TimeZone
+{
+public:
+   enum class TimeType
+   {
+      StandardTime = 0,
+      DaylightTime = 1,
+      GenericTime = 2
+   };
+   
+   enum class NameType 
+   {
+      DefaultName = 0,
+      LongName = 1,
+      ShortName = 2,
+      OffsetName = 3
+   };
+   
+   struct OffsetData
+   {
+      String m_abbreviation;
+      DateTime m_atUtc;
+      int m_offsetFromUtc;
+      int m_standardTimeOffset;
+      int m_daylightTimeOffset;
+   };
+   using OffsetDataList = std::vector<OffsetData>;
+   
+   TimeZone() noexcept;
+   explicit TimeZone(const ByteArray &ianaId);
+   explicit TimeZone(int offsetSeconds);
+   // implicit
+   TimeZone(const ByteArray &zoneId, int offsetSeconds, const String &name,
+            const String &abbreviation, Locale::Country country = Locale::Country::AnyCountry,
+            const String &comment = String());
+   TimeZone(const TimeZone &other);
+   ~TimeZone();
+   
+   TimeZone &operator=(const TimeZone &other);
+   TimeZone &operator=(TimeZone &&other) noexcept
+   {
+      swap(other);
+      return *this;
+   }
+   
+   void swap(TimeZone &other) noexcept
+   {
+      d.swap(other.d);
+   }
+   
+   bool operator==(const TimeZone &other) const;
+   bool operator!=(const TimeZone &other) const;
+   
+   bool isValid() const;
+   
+   ByteArray id() const;
+   Locale::Country country() const;
+   String comment() const;
+   
+   String displayName(const DateTime &atDateTime,
+                      TimeZone::NameType nameType = TimeZone::DefaultName,
+                      const Locale &locale = Locale()) const;
+   String displayName(TimeZone::TimeType timeType,
+                      TimeZone::NameType nameType = TimeZone::DefaultName,
+                      const Locale &locale = Locale()) const;
+   String abbreviation(const DateTime &atDateTime) const;
+   
+   int offsetFromUtc(const DateTime &atDateTime) const;
+   int standardTimeOffset(const DateTime &atDateTime) const;
+   int daylightTimeOffset(const DateTime &atDateTime) const;
+   
+   bool hasDaylightTime() const;
+   bool isDaylightTime(const DateTime &atDateTime) const;
+   
+   OffsetData offsetData(const DateTime &forDateTime) const;
+   
+   bool hasTransitions() const;
+   OffsetData nextTransition(const DateTime &afterDateTime) const;
+   OffsetData previousTransition(const DateTime &beforeDateTime) const;
+   OffsetDataList transitions(const DateTime &fromDateTime, const DateTime &toDateTime) const;
+   
+   static ByteArray systemTimeZoneId();
+   static TimeZone systemTimeZone();
+   static TimeZone utc();
+   
+   static bool isTimeZoneIdAvailable(const ByteArray &ianaId);
+   
+   static std::list<ByteArray> availableTimeZoneIds();
+   static std::list<ByteArray> availableTimeZoneIds(Locale::Country country);
+   static std::list<ByteArray> availableTimeZoneIds(int offsetSeconds);
+   
+   static ByteArray ianaIdToWindowsId(const ByteArray &ianaId);
+   static ByteArray windowsIdToDefaultIanaId(const ByteArray &windowsId);
+   static ByteArray windowsIdToDefaultIanaId(const ByteArray &windowsId,
+                                             Locale::Country country);
+   static std::list<ByteArray> windowsIdToIanaIds(const ByteArray &windowsId);
+   static std::list<ByteArray> windowsIdToIanaIds(const ByteArray &windowsId,
+                                                  Locale::Country country);
+   
+private:
+   TimeZone(TimeZonePrivate &dd);
+   friend class TimeZonePrivate;
+   friend class DateTime;
+   friend class DateTimePrivate;
+   pdk::utils::SharedDataPointer<TimeZonePrivate> m_imptr;
+};
+
+PDK_DECLARE_SHARED(TimeZone)
 
 } // time
 } // pdk
---
+++ include/pdk/utils/SharedData.h
@@ -0,0 +1,444 @@
+// @copyright 2017-2018 zzu_softboy <zzu_softboy@163.com>
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Created by softboy on 2018/01/30.
+
+#ifndef PDK_UTILS_SHARED_DATA_H
+#define PDK_UTILS_SHARED_DATA_H
+
+#include "pdk/global/Global.h"
+#include "pdk/base/os/thread/Atomic.h"
+#include "pdk/kernel/HashFuncs.h"
+
+namespace pdk {
+namespace utils {
+
+using pdk::os::thread::AtomicInt;
+
+template <typename T>
+class SharedDataPointer;
+
+class PDK_CORE_EXPORT SharedData
+{
+public:
+   mutable AtomicInt m_ref;
+   inline SharedData()
+      : m_ref(0)
+   {}
+   
+   inline SharedData(const SharedData &)
+      : ref(0) 
+   {}
+   
+private:
+   // using the assignment operator would lead to corruption in the ref-counting
+   SharedData &operator=(const SharedData &);
+};
+
+template <typename T>
+class SharedDataPointer
+{
+public:
+   using Type = T;
+   using pointer = T *;
+   using Pointer = pointer;
+   
+   inline void detach()
+   {
+      if (m_implPtr && m_implPtr->m_ref.load() != 1) {
+         detachHelper();
+      }
+   }
+   
+   inline T &operator*()
+   {
+      detach();
+      return *m_implPtr;
+   }
+   
+   inline const T &operator*() const
+   {
+      return *m_implPtr;
+   }
+   
+   inline T *operator->()
+   { 
+      detach();
+      return m_implPtr;
+   }
+   
+   inline const T *operator->() const
+   {
+      return m_implPtr;
+   }
+   
+   inline operator T *() 
+   {
+      detach();
+      return m_implPtr;
+   }
+   
+   inline operator const T *() const
+   {
+      return m_implPtr;
+   }
+   
+   inline T *data()
+   {
+      detach();
+      return m_implPtr;
+   }
+   
+   inline const T *data() const
+   {
+      return m_implPtr;
+   }
+   
+   inline const T *constData() const
+   {
+      return m_implPtr;
+   }
+   
+   inline bool operator==(const SharedDataPointer<T> &other) const
+   {
+      return m_implPtr == other.m_implPtr;
+   }
+   
+   inline bool operator!=(const SharedDataPointer<T> &other) const
+   {
+      return m_implPtr != other.m_implPtr;
+   }
+   
+   inline SharedDataPointer()
+   { 
+      m_implPtr = nullptr;
+   }
+   
+   inline ~SharedDataPointer()
+   {
+      if (m_implPtr && !m_implPtr->m_ref.deref()) {
+         delete m_implPtr;
+      }
+   }
+   
+   explicit SharedDataPointer(T *data) noexcept;
+   
+   inline SharedDataPointer(const SharedDataPointer<T> &other) 
+      : m_implPtr(other.m_implPtr)
+   {
+      if (m_implPtr) {
+         m_implPtr->m_ref.ref();
+      }
+   }
+   
+   inline SharedDataPointer<T> & operator=(const SharedDataPointer<T> &other)
+   {
+      if (other.m_implPtr != m_implPtr) {
+         if (other.m_implPtr) {
+            other.d->ref.ref();
+         }
+         T *old = m_implPtr;
+         m_implPtr = other.m_implPtr;
+         if (old && !old->m_ref.deref()) {
+            delete old;
+         }
+      }
+      return *this;
+   }
+   
+   inline SharedDataPointer &operator=(T *other) {
+      if (other != m_implPtr) {
+         if (other) {
+            other->m_ref.ref();
+         }
+         
+         T *old = m_implPtr;
+         m_implPtr = other;
+         if (old && !old->m_ref.deref()){
+            delete old;
+         }
+      }
+      return *this;
+   }
+   
+   SharedDataPointer(SharedDataPointer &&other) noexcept 
+      : m_implPtr(other.m_implPtr)
+   {
+      other.m_implPtr = nullptr;
+   }
+   
+   inline SharedDataPointer<T> &operator=(SharedDataPointer<T> &&other) noexcept
+   {
+      std::swap(m_implPtr, other.m_implPtr);
+      return *this;
+   }
+   
+   inline bool operator!() const { return !d; }
+   
+   inline void swap(SharedDataPointer &other) noexcept
+   {
+      std::swap(m_implPtr, other.m_implPtr);
+   }
+   
+protected:
+   T *clone();
+   
+private:
+   void detachHelper();
+   T *m_implPtr;
+};
+
+template <typename T> 
+class ExplicitlySharedDataPointer
+{
+public:
+   using Type = T;
+   using pointer = T *;
+   using Pointer = pointer;
+   
+   inline T &operator*() const
+   {
+      return *m_implPtr;
+   }
+   
+   inline T *operator->()
+   {
+      return m_implPtr;
+   }
+   
+   inline T *operator->() const
+   {
+      return m_implPtr;
+   }
+   
+   inline T *data() const
+   {
+      return m_implPtr;
+   }
+   
+   inline const T *constData() const
+   {
+      return m_implPtr;
+   }
+   
+   inline void detach()
+   {
+      if (m_implPtr && m_implPtr->m_ref.load() != 1) {
+         detachHelper(); 
+      }
+   }
+   
+   inline void reset()
+   {
+      if(m_implPtr && !m_implPtr->m_ref.deref()) {
+         delete m_implPtr;
+      }
+      m_implPtr = nullptr;
+   }
+   
+   inline operator bool () const 
+   { 
+      return m_implPtr != nullptr;
+   }
+   
+   inline bool operator==(const ExplicitlySharedDataPointer<T> &other) const
+   {
+      return m_implPtr == other.m_implPtr;
+   }
+   
+   inline bool operator!=(const ExplicitlySharedDataPointer<T> &other) const
+   {
+      return m_implPtr != other.m_implPtr;
+   }
+   
+   inline bool operator==(const T *ptr) const
+   {
+      return m_implPtr == ptr;
+   }
+   
+   inline bool operator!=(const T *ptr) const
+   {
+      return m_implPtr != ptr;
+   }
+   
+   inline ExplicitlySharedDataPointer()
+   {
+      m_implPtr = nullptr;
+   }
+   
+   inline ~ExplicitlySharedDataPointer()
+   {
+      if (m_implPtr && !m_implPtr->m_ref.deref()) {
+         delete m_implPtr;
+      }
+   }
+   
+   explicit ExplicitlySharedDataPointer(T *data) noexcept;
+   inline ExplicitlySharedDataPointer(const ExplicitlySharedDataPointer<T> &other)
+      : m_implPtr(other.m_implPtr) 
+   { 
+      if (other) {     
+         other->m_ref.ref();
+      }
+   }
+   
+   template<class X>
+   inline ExplicitlySharedDataPointer(const ExplicitlySharedDataPointer<X> &other)
+      : m_implPtr(static_cast<T *>(other.data()))
+   {
+      if(m_implPtr) {
+         m_implPtr->m_ref.ref();
+      }
+   }
+   
+   inline ExplicitlySharedDataPointer<T> & operator=(const ExplicitlySharedDataPointer<T> &other)
+   {
+      if (other.m_implPtr != m_implPtr) {
+         if (other.m_implPtr) {
+            other.d->m_ref.ref();
+         }
+         T *old = m_implPtr;
+         m_implPtr = other.m_implPtr;
+         if (old && !old->m_ref.deref()) {
+            delete old;
+         } 
+      }
+      return *this;
+   }
+   
+   inline ExplicitlySharedDataPointer &operator=(T *other) {
+      if (other != m_implPtr) {
+         if (other) {
+            other->m_ref.ref();
+         }
+         T *old = m_implPtr;
+         m_implPtr = other;
+         if (old && !old->m_ref.deref()) {
+            delete old;
+         }
+      }
+      return *this;
+   }
+   
+   inline ExplicitlySharedDataPointer(ExplicitlySharedDataPointer &&other) noexcept
+      : m_implPtr(other.m_implPtr)
+   { 
+      other.m_implPtr = nullptr;
+   }
+   
+   inline ExplicitlySharedDataPointer<T> &operator=(ExplicitlySharedDataPointer<T> &&other) noexcept
+   {
+      std::swap(m_implPtr, other.m_implPtr);
+      return *this;
+   }
+   
+   inline bool operator!() const
+   {
+      return !m_implPtr;
+   }
+   
+   inline void swap(ExplicitlySharedDataPointer &other) noexcept
+   {
+      std::swap(m_implPtr, other.m_implPtr);
+   }
+   
+protected:
+   T *clone();
+   
+private:
+   void detachHelper();
+   
+   T *m_implPtr;
+};
+
+template <typename T>
+inline SharedDataPointer<T>::SharedDataPointer(T *data) noexcept
+   : m_implPtr(data)
+{
+   if (m_implPtr) {
+      m_implPtr->m_ref.ref();
+   }
+}
+
+
+template <typename T>
+inline T *SharedDataPointer<T>::clone()
+{
+   return new T(*m_implPtr);
+}
+
+template <typename T>
+inline void SharedDataPointer<T>::detachHelper()
+{
+   T *x = clone();
+   x->m_ref.ref();
+   if (!m_implPtr->m_ref.deref()) {
+      delete m_implPtr;
+   }
+   m_implPtr = x;
+}
+
+template <typename T>
+inline T *ExplicitlySharedDataPointer<T>::clone()
+{
+   return new T(*d);
+}
+
+template <typename T>
+inline void ExplicitlySharedDataPointer<T>::detachHelper()
+{
+   T *x = clone();
+   x->ref.ref();
+   if (!m_implPtr->m_ref.deref()) {
+      delete m_implPtr;
+   }
+   m_implPtr = x;
+}
+
+template <typename T>
+inline ExplicitlySharedDataPointer<T>::ExplicitlySharedDataPointer(T *data) noexcept
+   : m_implPtr(data)
+{ 
+   if (m_implPtr){
+      m_implPtr->m_ref.ref();
+   }
+}
+
+template <typename T>
+inline void swap(SharedDataPointer<T> &lhs, SharedDataPointer<T> &rhs)
+{
+   lhs.swap(rhs);
+}
+
+template <typename T>
+inline void swap(ExplicitlySharedDataPointer<T> &lhs, ExplicitlySharedDataPointer<T> &rhs)
+{
+   lhs.swap(rhs);
+}
+
+template <typename T>
+inline uint hash(const SharedDataPointer<T> &ptr, uint seed = 0) noexcept
+{
+    return hash(ptr.data(), seed);
+}
+template <typename T>
+inline uint hash(const ExplicitlySharedDataPointer<T> &ptr, uint seed = 0) noexcept
+{
+    return hash(ptr.data(), seed);
+}
+
+} // utils
+} // pdk
+
+#endif // PDK_UTILS_SHARED_DATA_H
+
