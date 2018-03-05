fcbad6816d036004c2e3d78ebb1d7e2eabc79539
softboy <zzu_softboy@163.com>
2018-03-05 13:51:25 +0800

    add md4 third library

---
+++ thirdparty/md4/md4.h
@@ -0,0 +1,33 @@
+/*
+ * This is an OpenSSL-compatible implementation of the RSA Data Security,
+ * Inc. MD4 Message-Digest Algorithm.
+ *
+ * Written by Solar Designer <solar@openwall.com> in 2001, and placed in
+ * the public domain.  See md4.c for more information.
+ */
+
+#ifndef __MD4_H
+#define __MD4_H
+
+#include "pdk/global/Global.h"
+
+namespace pdk {
+namespace internal {
+
+#define	MD4_RESULTLEN (128/8)
+
+struct md4_context {
+	pdk::puint32 lo, hi;
+	pdk::puint32 a, b, c, d;
+	unsigned char buffer[64];
+	pdk::puint32 block[MD4_RESULTLEN];
+};
+
+static void md4_init(struct md4_context *ctx);
+static void md4_update(struct md4_context *ctx, const unsigned char *data, size_t size);
+static void md4_final(struct md4_context *ctx, unsigned char result[MD4_RESULTLEN]);
+
+} // internal
+} // pdk
+
+#endif
