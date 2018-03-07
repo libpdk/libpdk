382f9adc634cad6f40b5920588a4ed4e6a6f3dc8
softboy <zzu_softboy@163.com>
2018-03-07 23:13:56 +0800

    add pdk::kernel::BasicTimer

--- src/kernel/BasicTimer.cpp
+++ src/kernel/BasicTimer.cpp
@@ -16,21 +16,78 @@
 #include "pdk/kernel/BasicTimer.h"
 #include "pdk/kernel/AbstractEventDispatcher.h"
 #include "pdk/kernel/internal/AbstractEventDispatcherPrivate.h"
+#include "pdk/global/Logging.h"
 
 namespace pdk {
 namespace kernel {
 
+using internal::AbstractEventDispatcherPrivate;
+
 void BasicTimer::start(int msec, Object *obj)
 {
-   
+   AbstractEventDispatcher *eventDispatcher = AbstractEventDispatcher::getInstance();
+   if (PDK_UNLIKELY(!eventDispatcher)) {
+      warning_stream("BasicTimer::start: BasicTimer can only be used with threads started with Thread");
+      return;
+   }
+   if (PDK_UNLIKELY(obj && obj->getThread() != eventDispatcher->getThread())) {
+      warning_stream("BasicTimer::start: Timers cannot be started from another thread");
+      return;
+   }
+   if (m_id) {
+      if (PDK_LIKELY(eventDispatcher->unregisterTimer(m_id))) {
+         AbstractEventDispatcherPrivate::releaseTimerId(m_id);
+      } else {
+         warning_stream("BasicTimer::start: Stopping previous timer failed. Possibly trying to stop from a different thread");
+      }  
+   }
+   m_id = 0;
+   if (obj) {
+      m_id = eventDispatcher->registerTimer(msec, pdk::TimerType::CoarseTimer, obj);
+   }
 }
 
 void BasicTimer::start(int msec, pdk::TimerType timerType, Object *obj)
 {
+   AbstractEventDispatcher *eventDispatcher = AbstractEventDispatcher::getInstance();
+   if (PDK_UNLIKELY(msec < 0)) {
+      warning_stream("BasicTimer::start: Timers cannot have negative timeouts");
+      return;
+   }
+   if (PDK_UNLIKELY(!eventDispatcher)) {
+      warning_stream("BasicTimer::start: BasicTimer can only be used with threads started with Thread");
+      return;
+   }
+   if (PDK_UNLIKELY(obj && obj->getThread() != eventDispatcher->getThread())) {
+      warning_stream("BasicTimer::start: Timers cannot be started from another thread");
+      return;
+   }
+   if (m_id) {
+      if (PDK_LIKELY(eventDispatcher->unregisterTimer(m_id))) {
+         AbstractEventDispatcherPrivate::releaseTimerId(m_id);
+      } else {
+         warning_stream("BasicTimer::start: Stopping previous timer failed. Possibly trying to stop from a different thread");
+      }
+   }
+   m_id = 0;
+   if (obj) {
+      m_id = eventDispatcher->registerTimer(msec, timerType, obj);
+   }
 }
 
 void BasicTimer::stop()
 {
+   if (m_id) {
+      AbstractEventDispatcher *eventDispatcher = AbstractEventDispatcher::getInstance();
+      if (eventDispatcher) {
+         if (PDK_UNLIKELY(!eventDispatcher->unregisterTimer(m_id))) {
+            warning_stream("BasicTimer::stop: Failed. Possibly trying to stop from a different thread");
+            return;
+         }
+         AbstractEventDispatcherPrivate::releaseTimerId(m_id);
+      }
+   }
+   m_id = 0;
 }
 
 } // kernel
